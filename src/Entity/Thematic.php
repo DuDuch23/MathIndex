c99f16c9775248ee309fec7bff0b1458dd7c188d
Romain1298 <romainf039@gmail.com>
2024-01-10 14:29:49 +0100

    Création Entité Thematic et User

---
+++ src/Entity/Thematic.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ThematicRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ThematicRepository::class)]
+class Thematic
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Course $course_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCourseId(): ?Course
+    {
+        return $this->course_id;
+    }
+
+    public function setCourseId(?Course $course_id): static
+    {
+        $this->course_id = $course_id;
+
+        return $this;
+    }
+}
