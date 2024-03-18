a0f2a6668f928ea34c2bd12c612c24df7fbe9ee2
D.Alexandre <109027799+DuDuch23@users.noreply.github.com>
2024-03-18 09:36:09 +0100

    recherche exercice

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ClassroomRepository;
+use App\Repository\ExerciseRepository;
+use App\Repository\ThematicRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class RechercheController extends AbstractController
 {
     #[Route('/recherche', name: 'recherche')]
-    public function index(): Response
+    public function index(ExerciseRepository $exerciseRepository, ClassroomRepository $classroomRepository,
+    ThematicRepository $thematicRepository ): Response
     {
+        $classerooms = $classroomRepository->findAll();
+
+        $thematics = $thematicRepository->findAll();
+
+        $exercises = $exerciseRepository->findAll();
+
         return $this->render('recherche/index.html.twig', [
-            'controller_name' => 'RechercheController',
+            'exercises' => $exercises,
+            'classrooms' => $classerooms,
+            'thematics' => $thematics
         ]);
     }
 }
