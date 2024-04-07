e058fa5e79daec3f54709c7380aeeff2e99c59ef
D.Alexandre <109027799+DuDuch23@users.noreply.github.com>
2024-04-07 02:20:41 +0200

    affichage user et pagination

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,24 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('last_name')
-            ->add('first_name')
-            ->add('roles')
-            ->add('password')
+            ->add('email', EmailType::class, [
+
+            ])
+            ->add('last_name', TextType::class, [
+
+            ])
+            ->add('first_name', TextType::class, [
+
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'Contributeur' => 'ROLE_CONTRIBUTEUR',
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+
+            ])
         ;
     }
 
