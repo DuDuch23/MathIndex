f1cb328e7594571580a70704bd667a1e09eb5ae7
Killian Deletraz <killian.deletraz@hotmail.com>
2024-04-08 16:04:09 +0200

    ajout admin classes

---
+++ src/Form/ClassroomType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Classroom;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+
+class ClassroomType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Classroom::class,
+        ]);
+    }
+}
