0268e0728f2a514632696272c8a9e217760d976a
Killian Deletraz <killian.deletraz@hotmail.com>
2024-04-08 16:04:09 +0200

    ajout admin classes

--- src/Controller/Admin/ClassroomController.php
+++ src/Controller/Admin/ClassroomController.php
@@ -5,15 +5,81 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ClassroomRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Classroom;
+use App\Form\ClassroomType;
+use Doctrine\ORM\EntityManagerInterface;
+
+
 
 #[Route(path: '/panel', name: 'admin_panel_')]
 class ClassroomController extends AbstractController
 {
-    #[Route('/classroom', name: 'app_admin_classroom')]
-    public function index(): Response
+    #[Route('/classroom', name: 'classroom')]
+    public function index(ClassroomRepository $classroomRepository, Request $request): Response
     {
+        $classrooms = [];
+        $countPerPage = 8;
+        $countPages = 0;
+        $currentPage = $request->query->getInt('page', 1);
+
+        $classrooms = $classroomRepository->findAll();
+
         return $this->render('admin/classroom/index.html.twig', [
-            'controller_name' => 'AdminClassroomController',
+            'classrooms' => $classrooms,
+            'countPages' => $countPages,
+            'currentPage' => $currentPage,
+        ]);
+
+        
+            if($currentPage > $countPages || $currentPage <= 0)
+            {
+                throw $this->createNotFoundException();
+            }
+    }
+
+    #[Route(path: '/classroom/edit/{id}', name: 'classroom_edit')]
+    public function editClassroom(Request $request, classroom $classroom, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(classroomType::class, $classroom, [
+            'method' => 'POST',
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_panel_classroom', [
+                'id' => $classroom->getId(),
+            ]);
+        }
+        else{
+            $this->addFlash('error', 'Le formulaire contient des erreurs');
+        }
+
+        return $this->render('admin/classroom/edit.html.twig', [
+            'form' => $form,
+            'clasroom' => $classroom,
+            'edit_form' => $form->createView(),
         ]);
     }
+
+    #[Route('/classroom/delete/{id}', name: 'classroom_delete', methods: ['POST'])]
+    public function delete(Request $request, classroom $classroom, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$classroom->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($classroom);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La classe a été supprimée avec succès.');
+        } else {
+            $this->addFlash('error', 'Jeton CSRF invalide, impossible de supprimer la classe.');
+        }
+
+        return $this->redirectToRoute('admin_panel_classroom');
+    }
+
 }
