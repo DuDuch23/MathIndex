30eb3072eb29f25381ceed83be4817d617b51db9
Kilian11 <126764059+Nailik11@users.noreply.github.com>
2024-04-16 15:18:15 +0200

    mathematique

--- src/Controller/MathematiqueController.php
+++ src/Controller/MathematiqueController.php
@@ -4,18 +4,29 @@ namespace App\Controller;
 
 use App\Repository\ExerciseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MathematiqueController extends AbstractController
 {
     #[Route('/mathematique', name: 'mathematique')]
-    public function index(ExerciseRepository $exerciseRepository): Response
+    public function index(ExerciseRepository $exerciseRepository, Request $request): Response
     {
         $exercises = $exerciseRepository->findAll();
+        $nouveautes = $exerciseRepository->exerciseByThree();
+        $countPerPage = 8;
+        $countPages = 0;
+        $totalUsersFound = 0;
+        $currentPage = $request->query->getInt('page', 1);
+        $activatePaginate = false;
+
 
         return $this->render('mathematique/index.html.twig', [
             'exercises' => $exercises,
+            'nouveautes' => $nouveautes,
+            'countPages' => $countPages,
+            'currentPage' => $currentPage,
         ]);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    .enableVersioning(true)
 
     // configure Babel
     // .configureBabel((config) => {
