28ba1df14d566f9ee0db3cd6646c6e58e8a2b8d2
Kilian11 <126764059+Nailik11@users.noreply.github.com>
2024-04-22 10:59:21 +0200

    admin mathematique

--- src/Controller/Admin/ExerciceController.php
+++ src/Controller/Admin/ExerciceController.php
@@ -5,14 +5,28 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ExerciseRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route(path: '/panel', name: 'admin_panel_')]
 class ExerciceController extends AbstractController
 {
     #[Route(path: '/exercice', name: 'exercice')]
-    public function index(): Response
+    public function index(ExerciseRepository $exerciseRepository, Request $request): Response
     {
+        $exercises = $exerciseRepository->findAll();
+        $nouveautes = $exerciseRepository->exerciseByThree();
+        $countPerPage = 8;
+        $countPages = 0;
+        $totalUsersFound = 0;
+        $currentPage = $request->query->getInt('page', 1);
+        $activatePaginate = false;
+
         return $this->render('admin/exercice/index.html.twig', [
+            'exercises' => $exercises,
+            'nouveautes' => $nouveautes,
+            'countPages' => $countPages,
+            'currentPage' => $currentPage,
         ]);
     }
 }
