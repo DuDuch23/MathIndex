87640391381e6c8870d925ee16a55cbaa43937c3
Kilian11 <126764059+Nailik11@users.noreply.github.com>
2024-04-22 17:22:24 +0200

    gt

---
+++ src/Form/CourseType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+        {
+            $builder
+                ->add('name', TextType::class, [
+                    'label' => 'Nom :',
+                    'constraints' => [
+                        new NotBlank(),
+                        new Length([
+                            'min' => 2,
+                            'max' => 255,
+                            'minMessage' => 'Le nom doit contenir {{ limit }} caractères au minimum.',
+                            'maxMessage' => 'Le nom ne doit pas excéder {{ limit }} caractères.',
+                        ]),
+                        new Regex([
+                            'pattern' => '/^[a-zA-Z]+$/',
+                            'message' => 'Le nom ne doit contenir que des lettres.',
+                        ]),
+                    ],
+                ])
+                ->add('submit', SubmitType::class, [
+                    'label' => 'Envoyer',
+                    'attr' => [
+                        'class' => 'btn',
+                    ],
+                ])
+            ;
+            ;
+        }
+}
